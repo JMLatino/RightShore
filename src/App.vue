a3d611e2552bd333c3cfd16c574fe8e01e17b5f4
Juan Besselse <juan.besselse.navas@nl.abnamro.com>
2022-07-29 19:08:38 +0200

    Added primary inputs

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,8 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
-
-    <div class="">
-      <!-- <HelloWorld msg="You did it!" /> -->
-    </div>
-  </header>
-
-  <main>
-    <div class="container shadow p-3 mb-5 bg-body rounded">
-
+  <main class="container shadow p-3 mb-5 bg-body rounded">
+    <div>
       <div class="accordion" id="accordionExample">
         <div class="accordion-item">
           <h2 class="accordion-header" id="headingOne">
@@ -52,43 +43,78 @@ import TheWelcome from './components/TheWelcome.vue'
                 <label for="time-to-market" class="form-label">Governance</label>
                 <input type="email" class="form-control" id="time-to-market">
               </div>
-
             </div>
           </div>
         </div>
         <div class="accordion-item">
           <h2 class="accordion-header" id="headingTwo">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Accordion Item #2
+              data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"> Project Characteristics
             </button>
           </h2>
           <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
             data-bs-parent="#accordionExample">
             <div class="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse
-              plugin adds the appropriate classes that we use to style each element. These classes control the overall
-              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
-              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-              <code>.accordion-body</code>, though the transition does limit overflow.
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Size</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Project Timeline</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">CIA Ratings</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Compliance/regulatory</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Domain Intense</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">New age technology</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Packaged Solution</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Legacy Dominance</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
             </div>
           </div>
         </div>
         <div class="accordion-item">
           <h2 class="accordion-header" id="headingThree">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-              data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-              Accordion Item #3
+              data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"> Sourcing Requirement
             </button>
           </h2>
           <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
             data-bs-parent="#accordionExample">
             <div class="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse
-              plugin adds the appropriate classes that we use to style each element. These classes control the overall
-              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
-              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
-              <code>.accordion-body</code>, though the transition does limit overflow.
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Complexity</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Ease of collaboration</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Cultural Alignment</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Incumbent Transition</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
             </div>
           </div>
         </div>
@@ -98,30 +124,4 @@ import TheWelcome from './components/TheWelcome.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
