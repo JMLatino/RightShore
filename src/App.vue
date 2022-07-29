1b33d58fade757c35a4ef033318790166735ba05
Juan Besselse <juan.besselse.navas@nl.abnamro.com>
2022-07-29 18:45:43 +0200

    first setup

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+
+    <div class="">
+      <!-- <HelloWorld msg="You did it!" /> -->
+    </div>
+  </header>
+
+  <main>
+    <div class="container shadow p-3 mb-5 bg-body rounded">
+
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+              aria-expanded="true" aria-controls="collapseOne"> Design Principles </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Time to market</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Cost Consideration</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Quality</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Process Maturity</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Innovative delivery</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Sustainability</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+              <div class="mb-3">
+                <label for="time-to-market" class="form-label">Governance</label>
+                <input type="email" class="form-control" id="time-to-market">
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Accordion Item #2
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse
+              plugin adds the appropriate classes that we use to style each element. These classes control the overall
+              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
+              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+              <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Accordion Item #3
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse
+              plugin adds the appropriate classes that we use to style each element. These classes control the overall
+              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
+              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+              <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
