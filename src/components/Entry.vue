74c16828116d51385670bf4530554f6fd33b9b93
Janiszewska <katarzyna.janiszewska@capgemini.com>
2022-08-05 12:09:47 +0200

    name per group in radio button group

--- src/components/Entry.vue
+++ src/components/Entry.vue
@@ -5,7 +5,7 @@
     </span>
     <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
       <template v-for="(answer, answerId) in entry.answers" :key="answer">
-        <input type="radio" class="btn-check" name="btnradio" :id="`${entry.id}-${answerId}`" autocomplete="off"
+        <input type="radio" class="btn-check" :name="`btnradio-${entry.id}`" :id="`${entry.id}-${answerId}`" autocomplete="off"
         @click="setAnswer(entry.id, answerId)" @change="(e) => markChecked(e)">
         <label class="btn btn-outline-primary" :for="`${entry.id}-${answerId}`">{{ answer }}</label>
       </template>
