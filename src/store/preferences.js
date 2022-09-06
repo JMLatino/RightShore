0f4578160725b6dfa63242c09cfb3e2f20a6df52
Janiszewska <katarzyna.janiszewska@capgemini.com>
2022-09-06 18:29:42 +0200

    improvements to counter

--- src/store/preferences.js
+++ src/store/preferences.js
@@ -29,6 +29,12 @@ export const usePreferencesStore = defineStore({
         });
       });
     },
+    clearForm() {
+      this.preferences.forEach(category => category.entries.forEach((question) => {
+        delete question.answered;
+        return question;
+      }))
+    },
     toggleCoffee() {
       this.isCoffee = true;
     },
